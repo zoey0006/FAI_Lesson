5ebd9409e29a3eae61596de5b40778929d07d026
Teoh TeikToe <teohteiktoe@gmail.com>
2025-02-13 08:02:12 +0000

    1

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    return(render_template("prediction.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r=(-50.6*q)+90.2))
+
 if __name__ == "__main__":
     app.run()
