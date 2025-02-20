caae242c7b0c460d71bfc59d6909b48ff5ad737d
Teoh TeikToe <teohteiktoe@gmail.com>
2025-02-20 08:11:34 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request,render_template
+import textblob
 
 app = Flask(__name__)
 
@@ -15,6 +16,16 @@ def prediction():
 def dbs():
     q = float(request.form.get("q"))
     return(render_template("dbs.html", r=(-50.6*q)+90.2))
+
+@app.route("/sentiment_analysis",methods=["GET","POST"])
+def sentiment_analysis():
+    return(render_template("sentiment_analysis.html"))
+
+@app.route("/sentiment_result",methods=["GET","POST"])
+def sentiment_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("sentiment_result.html", r=r))
 
 if __name__ == "__main__":
     app.run()
